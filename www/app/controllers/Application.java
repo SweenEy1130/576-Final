915ed6d3a5a661adbf7fe50f2b18fe189de1bd5b
SweenEy <ronnie.alonso@gmail.com>
2014-12-07 01:02:27 -0800

    Add photo and show cartoon & facedetect

--- www/app/controllers/Application.java
+++ www/app/controllers/Application.java
@@ -1,31 +1,52 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import utils.FileFinder;
+import play.Logger;
 
 public class Application extends Controller {
 
     public static Result welcome() {
         String path = "public/img/", pattern = "\\S+\\.jpg";
-
-        FileFinder ff = new FileFinder();
-        ArrayList<String> filenames = ff.find(path, pattern);
+        ArrayList<String> filenames = FileFinder.find(path, pattern);
 
         return ok(welcome.render(filenames));
     }
 
-    public static Result slide() {
-        String path = "public/img/", pattern = "\\S+\\.jpg";
-
-        FileFinder ff = new FileFinder();
-        ArrayList<String> filenames = ff.find(path, pattern);
-
+    public static Result slide(){
+    	String path = "public/img/", pattern = "\\S+\\.jpg";
+        ArrayList<String> filenames = FileFinder.find(path, pattern);
+        return ok(slide.render(filenames));
+    }
+    
+    public static Result faceSlide() throws IOException {
+    	ArrayList<String> filenames = new ArrayList<String>();
+        ArrayList<Boolean> selector = FileFinder.JSONParse("faceDetect.json");
+        Logger.debug(Integer.toString(selector.size()));
+        for (int i = 0; i < selector.size(); i++){
+        	Logger.debug(Boolean.toString(selector.get(i)));
+        	if (selector.get(i)){
+        		filenames.add((i+1) + ".jpg");
+        	}
+        }
+        return ok(slide.render(filenames));
+    }
+    
+    public static Result cartoonSlide() throws IOException {
+    	ArrayList<String> filenames = new ArrayList<String>();
+        ArrayList<Boolean> selector = FileFinder.JSONParse("cartoon.json");
+        Logger.debug(Integer.toString(selector.size()));
+        for (int i = 0; i < selector.size(); i++){
+        	Logger.debug(Boolean.toString(selector.get(i)));
+        	if (selector.get(i)){
+        		filenames.add((i+1) + ".jpg");
+        	}
+        }
         return ok(slide.render(filenames));
     }
 }
--- www/app/utils/FileFinder.java
+++ www/app/utils/FileFinder.java
@@ -4,27 +4,46 @@ import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class FileFinder {
-    public FileFinder(){
-        ans = new ArrayList<String>();
-    }
-    public ArrayList<String> ans;
-    public ArrayList<String> find(String path,String reg){
-        Pattern pat = Pattern.compile(reg);
-        File file = new File(path);
-        File[] arr = file.listFiles();
-        for(int i = 0;i < arr.length;i++){
-            if(arr[i].isDirectory()){
-                find(arr[i].getAbsolutePath(), reg);
-            }
+	public static ArrayList<String> find(String path, String reg){
+		ArrayList<String> ans = new ArrayList<String>();
+		Pattern pat = Pattern.compile(reg);
+		File file = new File(path);
+		File[] arr = file.listFiles();
+		for(int i = 0;i < arr.length;i++){
+			Matcher mat = pat.matcher(arr[i].getAbsolutePath());
 
-            Matcher mat = pat.matcher(arr[i].getAbsolutePath());
+			if(mat.matches()){
+				ans.add(arr[i].getName());
+			}
+		}
+		return ans;
+	}
 
-            if(mat.matches()){
-                ans.add(arr[i].getName());
-            }
-        }
-        return ans;
-    }
+	public static ArrayList<Boolean> JSONParse(String selector) throws IOException{
+		InputStream    fis;
+		BufferedReader br;
+
+		fis = new FileInputStream("label/" + selector);
+		br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+		String arr = br.readLine();
+		String[] items = arr.replaceAll("\\[", "").replaceAll("\\]", "").split(",");
+		ArrayList<Boolean> results = new ArrayList<Boolean>();
+
+		for (int i = 0; i < items.length; i++) {
+		    try {
+		    	if (items[i].equals("true"))
+		    		results.add(true);
+		    	else 
+		    		results.add(false);
+		    } catch (NumberFormatException nfe) {};
+		}
+
+		// Done with the file
+		br.close();
+		return results;
+	}
 }
